29e4daff2ed439ae36977e601c585aa2b4aae402
toririn <toririn.paftako@gmail.com>
2016-08-28 04:19:49 +0000

    add base

--- lib/simple_mattermost.rb
+++ lib/simple_mattermost.rb
@@ -1,4 +1,9 @@
+require "httpclient"
+require "json"
 require "simple_mattermost/version"
+require "simple_mattermost/constants"
+require "simple_mattermost/client"
+require "simple_mattermost/hook"
 
 module SimpleMattermost
   # Your code goes here...
---
+++ lib/simple_mattermost/constants.rb
@@ -0,0 +1,6 @@
+module SimpleMattermost
+  API_PATH          = '/api/v3'
+  LOGIN_PATH        = '/users/login'
+  INITIAL_LOAD_PATH = '/users/initial_load'
+  MY_INFO_PATH      = '/users/me'
+end
